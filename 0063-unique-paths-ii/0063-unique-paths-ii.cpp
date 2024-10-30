0016c9e12ed5639d7d883b1af7e234c6bf98f00f
stoicinnature <stoicinnature@gmail.com>
2024-10-30 22:57:42 +0530

    Time: 0 ms (100%), Space: 10.4 MB (81.61%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int f(int x, int y, vector<vector<int>>& grid, vector<vector<int>> &memo){
-        if(x == 0 && y == 0) return 1;
-        if(x < 0 || y < 0) return 0;
-        if(memo[x][y] != -1) return memo[x][y];
-
-        int up = 0, left = 0;
-        if(x > 0 && grid[x - 1][y] != 1) up = f(x -1, y, grid, memo);
-        if(y > 0 && grid[x][y - 1] != 1) left = f(x, y - 1, grid, memo);
-        return memo[x][y] = up + left;
-    }
+   
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        //  Let's do bottom up and memoization
+        //  Let's do bottom up using tabulation
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
         vector<vector<int>> memo(m, vector<int>(n, -1));
-        if(obstacleGrid[m-1][n-1] == 1) return 0;
-        return f(m - 1, n - 1, obstacleGrid, memo);
+        if (obstacleGrid[m - 1][n - 1] == 1) return 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) memo[i][j] = 1;
+                else {
+                    int up = 0, left = 0;
+                    if (i > 0 && obstacleGrid[i - 1][j] != 1) up = memo[i - 1][j];
+                    if (j > 0 && obstacleGrid[i][j - 1] != 1) left = memo[i][j - 1];
+                    memo[i][j] = up + left;
+                }
+            }
+        }
+        return memo[m - 1][n - 1];
     }
 };
