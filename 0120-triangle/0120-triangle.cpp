83076a3b582109bee80c7a78c41b4d7ce60e6383
stoicinnature <stoicinnature@gmail.com>
2024-10-31 11:27:52 +0530

    Time: 0 ms (100%), Space: 11.8 MB (33.75%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int f(int i, int j, int m, vector<vector<int>>& triangle, vector<vector<int>> &dp){
-        if(j >= triangle[i].size() || j < 0) return INT_MAX;
-        if(i == m - 1) return triangle[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int down = 0, rightDownCorner = 0;
-        down = f(i + 1, j, m, triangle, dp);
-        rightDownCorner = f(i + 1,j + 1,m,triangle, dp);
-
-        return dp[i][j] = triangle[i][j] + min({down, rightDownCorner});
-    }
+   
     int minimumTotal(vector<vector<int>>& triangle) {
-        // Let's do  Bottom Up memoization
+        // Let's do  Bottom Up Tabulation
         int m = triangle.size();
-        vector<vector<int>> dp(m,vector<int>(m, -1));
-        return f(0,0,m,triangle, dp);
+        vector<vector<int>> dp(m,vector<int>(m, INT_MAX));
+        for(int i=0; i<m; i++){
+            for(int j=0; j<=i; j++){
+                if(i == 0 && j == 0) dp[i][j] = triangle[0][0];
+                else{
+                    int top = INT_MAX, topLeftCorner = INT_MAX;
+                    if(i > 0) top = dp[i - 1][j];
+                    if(i > 0 && j > 0) topLeftCorner = dp[i-1][j-1];
+                    dp[i][j] = triangle[i][j] + min({top, topLeftCorner});
+                }
+            }
+        }
+        return *min_element(dp[m-1].begin(), dp[m-1].end());
     }
 };
