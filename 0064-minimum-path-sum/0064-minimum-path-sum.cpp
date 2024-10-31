8f3bac72557912d674eae88e8e33c59028dd8b5f
stoicinnature <stoicinnature@gmail.com>
2024-10-31 07:46:43 +0530

    Time: 0 ms (100%), Space: 15 MB (27.1%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int f(int x, int y, vector<vector<int>>& grid, vector<vector<int>> &dp){
-        if(x >= grid.size() || y >= grid[0].size()) return INT_MAX;
-        if (x == grid.size() - 1 && y == grid[0].size() - 1) return grid[x][y];
-        if(dp[x][y] != -1) return dp[x][y];
-
-        //Since left and top movement will increase path sum unnecessary
-        int right = 0,down = 0;
-        right = f(x, y+1, grid,dp);
-        down = f(x + 1, y, grid, dp);
-        return dp[x][y] =  grid[x][y] + min(right, down);
-    }
+   
     int minPathSum(vector<vector<int>>& grid) {
-        //Bottom up with memoization
+        //Bottom up with Tabulation
         vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), -1));
-        return f(0,0,grid, dp);
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                if(i == 0 && j == 0) dp[i][j] = grid[0][0];
+                else{
+                    int left = INT_MAX,top = INT_MAX;
+                    if(j > 0) left = dp[i][j-1];
+                    if(i > 0) top = dp[i-1][j];
+                    dp[i][j] = grid[i][j] + min(left,top);
+                }
+            }
+        }
+        return dp[grid.size() - 1][grid[0].size() - 1];
     }
 };
