2d8ba86a05e87e39de49bc84f49842b30b96b100
stoicinnature <stoicinnature@gmail.com>
2024-11-05 08:03:16 +0530

    Time: 3 ms (6.7%), Space: 28 MB (5.13%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        stack<int> st;
+        stack<char> st;
         string res;
         for(int i=0; i<s.size(); i++) st.push(s[i]);
         int i=0;
